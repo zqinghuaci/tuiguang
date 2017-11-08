db03e98cdff67c27e0499427bf7d7a490c8eed28
zqinghuaci <2968659560@qq.com>
2017-11-08 14:35:38 +0800

    提交注释

--- wenane/js/wxnume.js
+++ wenane/js/wxnume.js
@@ -1,11 +1,5 @@
-/*
-arr_wx=['jkkk5883','yzb8668','fsb8686','18008645463','w512018888','18008655664'];
-var wx_index = Math.floor((Math.random()*arr_wx.length));
-var stxlwx = arr_wx[wx_index];
-*/
 $(function(){
 	var wxnum = ['jkkk5883','yzb8668','fsb8686','18008645463','w512018888','18008655664'];
 		wxIndex = parseInt(Math.random()*wxnum.length);
 		$(".wxnumsje").text(wxnum[wxIndex]);
-		console.log(wxIndex);
 })
